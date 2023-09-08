bd4951c7d2d630f665e3cca6c3955236a5ec083f
dolsen0 <derek.olsen@gmail.com>
2023-09-07 21:50:58 -0400

    added frozen pizza section

---
+++ src/app/components/Frozen.jsx
@@ -0,0 +1,11 @@
+export default function Frozen() {
+  return (
+    <div className="frozen__text">
+      <h2>New Offer</h2>
+      <p>
+        We are now offering fresh, flash frozen pizza for you to bring home and
+        reheat yourself. Get the second best pizza in town, in your own home!
+      </p>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import GetTitle from "./components/GetTitle";
 import GetMenu from "./components/GetMenu";
 import GetLocation from "./components/GetLocation";
 import AboutUs from "./components/AboutUs";
+import Frozen from "./components/Frozen";
 
 export default function Home() {
   return (
@@ -20,6 +21,9 @@ export default function Home() {
         </section>
         <section>
           <GetLocation />
+        </section>
+        <section className="frozen">
+          <Frozen />
         </section>
       </div>
     </>
